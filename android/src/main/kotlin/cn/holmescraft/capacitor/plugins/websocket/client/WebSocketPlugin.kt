8814836b98e9f70ceca392ae91dca9fb471dd660
Ordinary <35167795+OrdinarySF@users.noreply.github.com>
2026-08-15 02:39:02 +0000

    fix: avoid Android NPE when websocket callbacks are gone after App.exit

--- android/src/main/kotlin/cn/holmescraft/capacitor/plugins/websocket/client/WebSocketPlugin.kt
+++ android/src/main/kotlin/cn/holmescraft/capacitor/plugins/websocket/client/WebSocketPlugin.kt
@@ -78,6 +78,11 @@ class WebSocketPlugin : Plugin() {
         WebSocket.onFailure(id, call.callbackId)
     }
 
+    override fun handleOnDestroy() {
+        WebSocket.reset()
+        super.handleOnDestroy()
+    }
+
     private fun getStringForCall(call: PluginCall, name: String): String? {
         val id = call.getString(name)
         if (id == null) call.reject("$name can not null.");
